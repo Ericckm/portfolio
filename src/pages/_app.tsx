b82e3f5db245dea67def55c18632fe57e2bdd74a
Erick <ericckm@outlook.com.br>
2022-08-17 11:01:42 -0300

    add metatag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import { AppProps } from 'next/app'
 import GlobalStyle from '../styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
+import Head from 'next/head'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ const Home = () => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="/assets/favicon.png" />
         <title>Ericckm</title>
       </Head>
