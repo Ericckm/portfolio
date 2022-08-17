ccae135570931aefaa37f8092e8b443e5062af41
Erick <ericckm@outlook.com.br>
2022-08-17 11:43:54 -0300

    change menu item name

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -6,11 +6,7 @@ export const Menu = ({ menuOpen, setMenuOpen }) => {
     <MenuContainer className={menuOpen && 'open'}>
       <ul>
         <MenuList href="#intro" title="Home" setMenuOpen={setMenuOpen} />
-        <MenuList
-          href="#portfolio"
-          title="Portfolio"
-          setMenuOpen={setMenuOpen}
-        />
+        <MenuList href="#project" title="Project" setMenuOpen={setMenuOpen} />
         <MenuList href="#skills" title="Skills" setMenuOpen={setMenuOpen} />
         <MenuList href="#contact" title="Contact" setMenuOpen={setMenuOpen} />
       </ul>
